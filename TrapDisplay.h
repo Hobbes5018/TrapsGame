e2009d92a89d12189da810f68472e9d65e386045
Hobbes5018 <85383872+Hobbes5018@users.noreply.github.com>
2022-06-04 21:18:39 +0200

    Delete TrapDisplay.h

--- TrapDisplay.h
+++
@@ -1,9 +0,0 @@
-#ifndef TRAPDISPLAY_H
-#define TRAPDISPLAY_H
-
-#include "TrapHeaders.h"
-
-extern int TrapDispSpriteAlt(HobbesSprite *sprite, int flipmode, double angle);
-extern int TrapDispSprite(HobbesSprite *sprite);
-
-#endif
